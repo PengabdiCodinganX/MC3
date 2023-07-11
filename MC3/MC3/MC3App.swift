5f05a1b1c50f76df15bf9cc7f3843a2014f541ec
Vincent Gunawan <gnwnvincent@gmail.com>
2023-07-11 10:39:01 +0700

    Initial commit

---
+++ MC3/MC3/MC3App.swift
@@ -0,0 +1,17 @@
+//
+//  MC3App.swift
+//  MC3
+//
+//  Created by Vincent Gunawan on 11/07/23.
+//
+
+import SwiftUI
+
+@main
+struct MC3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
