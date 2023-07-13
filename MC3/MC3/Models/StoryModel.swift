3fef1164b7d4b64faf1663ffd89b4fd79e84e039
Muhammad Adha Fajri Jonison <madhafajrij@gmail.com>
2023-07-13 09:59:36 +0700

    add files in model and view models folder

---
+++ MC3/MC3/Models/StoryModel.swift
@@ -0,0 +1,8 @@
+//
+//  StoryModel.swift
+//  MC3
+//
+//  Created by Muhammad Adha Fajri Jonison on 13/07/23.
+//
+
+import Foundation
---
+++ MC3/MC3/ViewModels/AuthViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AuthViewModel.swift
+//  MC3
+//
+//  Created by Muhammad Adha Fajri Jonison on 13/07/23.
+//
+
+import Foundation
