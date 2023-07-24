0be8ec73cbe158cfb1bbeca8df1a938dde06c459
Muhammad Adha Fajri Jonison <madhafajrij@gmail.com>
2023-07-24 17:08:56 +0700

    minor fix

--- MC3/MC3/Presenter/Views/Home/HomeView.swift
+++ MC3/MC3/Presenter/Views/Home/HomeView.swift
@@ -13,65 +13,42 @@ struct HomeView: View {
     @EnvironmentObject var pathStore: PathStore
     
     @State private var text: String = ""
+    @State var audioPlayer: AVAudioPlayer?
     
     @Binding var isSignedIn: Bool
     
-    @State var audioPlayer: AVAudioPlayer?
-    
     var body: some View {
         VStack {
-//            Mascot(text: "Good afternoon, dwq! What would you like to do?", alignment: .horizontal)
-//
-//            MenuButton(
-//                text: "Share your story, Find relief!",
-//                menuButtonType: .big
-//            ) {
-//
-//            }
-//
-//            HStack {
-//                MenuButton(
-//                    text: "Share your story, Find relief!",
-//                    menuButtonType: .big
-//                ) {
-//
-//                }
-//
-//                VStack {
-//                    MenuButton(
-//                        text: "Share your story, Find relief!",
-//                        menuButtonType: .small
-//                    ) {
-//
-//                    }
-//
-//                    MenuButton(
-//                        text: "Share your story, Find relief!",
-//                        menuButtonType: .small
-//                    ) {
-//
-//                    }
-//                }
-//            }
+            Mascot(text: "Good afternoon, dwq! What would you like to do?", alignment: .horizontal)
             
-            SingleTextField(placeholder: "Input text", text: $text)
-            PrimaryButton(text: "Save") {
-                Task {
-                    try await viewModel.saveSound(text: text)
-                }
+            MenuButton(
+                text: "Share your story, Find relief!",
+                menuButtonType: .big
+            ) {
+                
             }
             
-            if !viewModel.soundList.isEmpty {
-                List(viewModel.soundList, id: \.self) { sound in
-                    Button {
-                        guard sound.sound != nil else {
-                            print("[sound.sound]", sound.sound)
-                            return
-                        }
+            HStack {
+                MenuButton(
+                    text: "Share your story, Find relief!",
+                    menuButtonType: .big
+                ) {
+                    
+                }
+                
+                VStack {
+                    MenuButton(
+                        text: "Share your story, Find relief!",
+                        menuButtonType: .small
+                    ) {
                         
-                            playSound(data: sound.sound!)
-                    } label: {
-                        Text(sound.text ?? "")
+                    }
+                    
+                    MenuButton(
+                        text: "Share your story, Find relief!",
+                        menuButtonType: .small
+                    ) {
+                        
                     }
                 }
             }
@@ -86,32 +63,18 @@ struct HomeView: View {
                     
                 }
                 
-            }
-            
-            ToolbarItem(placement: .navigationBarTrailing) {
-                Button("Second") {
-                    print("Pressed")
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Second") {
+                        print("Pressed")
+                    }
                 }
             }
         }
     }
-    
-    func playSound(data: Data) {
-        print("[playSound][data]", data)
-        do {
-            audioPlayer = try AVAudioPlayer(data: data)
-            print("[playSound][audioPlayer]", audioPlayer)
-            audioPlayer?.prepareToPlay()
-            audioPlayer?.play()
-            print("[playSound][playing]")
-        } catch {
-            print("[playSound][error]", error)
-        }
-    }
 }
 
-//struct HomeView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        HomeView()
-//    }
-//}
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView(isSignedIn: .constant(false))
+    }
+}
